c51984599cb9d87bdaf7264ebfe23ad82fad72ba
MikielAgutu <mikiel.agutu@red-gate.com>
2018-02-02 13:40:37 +0000

    Implement actions

--- app6/app/src/modules/products.js
+++ app6/app/src/modules/products.js
@@ -8,3 +8,72 @@ const PRODUCT_REMOVE_COMPLETED = 'products/PRODUCT_REMOVE_COMPLETED';
 export default (state = initialState, action) => {
   return state;
 }
+
+function fetchProducts() {
+    return dispatch => {
+        dispatch({ type: PRODUCTS_REQUESTED })
+        const url = '/api/products/get'
+        fetch(url, {
+            method: 'GET',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            credentials: 'same-origin'
+        }).then(response => {
+            return response.json()
+        }).then(json => {
+            dispatch({
+            type: PRODUCTS_COMPLETED,
+            payload: { product: json }
+            })
+        })
+    }
+}
+
+function addProduct(product) {
+    const formData = new FormData();
+    formData.append( "json", JSON.stringify(product));
+
+    return dispatch => {
+        dispatch({ type: PRODUCT_ADD_REQUESTED })
+        const url = '/api/products/add'
+        fetch(url, {
+            method: 'POST',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            credentials: 'same-origin',
+            body: formData
+        }).then(response => {
+            return response.json()
+        }).then(json => {
+            dispatch({
+            type: PRODUCT_ADD_COMPLETED
+            })
+        })
+    }
+}
+
+function removeProduct(productName) {
+    return dispatch => {
+        dispatch({ type: PRODUCT_REMOVE_REQUESTED })
+        const url = '/api/products/delete/' + productName
+        fetch(url, {
+            method: 'DELETE',
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            credentials: 'same-origin'
+        }).then(response => {
+            return response.json()
+        }).then(json => {
+            dispatch({
+            type: PRODUCT_REMOVE_COMPLETED,
+            payload: { product: json }
+            })
+        })
+    }
+}
